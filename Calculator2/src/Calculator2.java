65c5ece6a3396c925015d2e0bbebef3d37e0f895
nety2k <nety2k@hanmail.net>
2017-01-25 09:47:12 +0900

    1056a62945444748d71a5825995e91bd368e2599

--- Calculator2/src/Calculator2.java
+++ Calculator2/src/Calculator2.java
@@ -1,19 +1,6 @@
 
 // Add 1 # add2 #add3 #add4 #5 new add 1 #6 new add2
-
 // B add
-
-// A add
-
-
-// B 2 add
-
-// A 2 add
-
-// add 2017/01/23
-// add 2017/01/23 -1
-// add 01/23 - ø¿¥√ 10Ω√¡ﬂ..
-// 01/23 - 4
 public class Calculator2 {
 
 }
