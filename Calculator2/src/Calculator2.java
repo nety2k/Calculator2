7d3458a571e8acc87a185cdcd8304bd29fbc3dfa
nety2k <nety2k@hanmail.net>
2017-02-01 09:58:53 +0900

    마스터 브랜치 hello world 로직변경

--- Calculator2/src/Calculator2.java
+++ Calculator2/src/Calculator2.java
@@ -2,8 +2,8 @@
 // Add 1 # add2 #add3 #add4 #5 new add 1 #6 new add2
 // B add
 public class Calculator2 {
-	public void helloworld(int code) {
-		
+	public void helloworld(float code) {
+		// 1, 2, 3, 4, 5
 	}
 
 }
