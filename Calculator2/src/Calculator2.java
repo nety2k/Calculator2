8492840826dd95ddee6a982ba37f7777a12db09b
nety2k <nety2k@hanmail.net>
2017-02-01 09:56:26 +0900

    change int code to char  로직 변경

--- Calculator2/src/Calculator2.java
+++ Calculator2/src/Calculator2.java
@@ -2,8 +2,8 @@
 // Add 1 # add2 #add3 #add4 #5 new add 1 #6 new add2
 // B add
 public class Calculator2 {
-	public void helloworld(int code) {
-		
+	public void helloworld(char code) {
+		//로직 추가 예정
 	}
 
 }
